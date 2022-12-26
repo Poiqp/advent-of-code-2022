544f50570310b866686bb7f4c2ee70c6f5da6254
Poiqp <p.trociuk@gmail.com>
2022-12-26 14:53:25 +0100

    day 9 changes

--- day_9/main.py
+++ day_9/main.py
@@ -1,24 +1,67 @@
+import math 
+
 input = 'test_data.txt'
-input = 'real_data.txt'
+# input = 'real_data.txt'
 
 with open(input, 'r') as f:
-    data = f.readline()
+    data = f.readlines()
+    data = [x.replace('\n', '') for x in data]
+
+class Pin:
+    def __init__(self,x,y) -> None:
+        self.x = x 
+        self.y = y 
+    
+    def move(self, direction: str):
+        match direction:
+            case 'R':
+                self.x += 1 
+            case 'L':
+                self.x -= 1
+            case 'U':
+                self.y += 1
+            case 'D':
+                self.y -= 1
+
+    
+class Rope:
+    def __init__(self) -> None:
+        self.head = Pin(0,0)
+        self.tail = Pin(0,0)
+        self.visited = []
+    
+    def is_close(self) -> bool:
+        if math.sqrt((self.head.x - self.tail.x)**2 + (self.head.y - self.tail.y)**2) > 1:
+            return False
+        else:
+            return True
+
+    def move_and_follow(self, direction: str, steps: int ) -> None:
+        for step in range(0, steps+1):
+            self.head.move(direction)
+            print("H:  " + str(self.head.x) + "  " + str(self.head.y) )
+            print("T:  " + str(self.tail.x) + "  " + str(self.tail.y) )
+            if not self.is_close():
+                self.tail.move(direction)
+                print("T:  " + str(self.tail.x) + "  " + str(self.tail.y) )
 
 
+
+
+    
 def part_one(data):
-    for letter in range(4, len(data)):
-        if len(set(data[letter-4:letter])) == 4:
-            return letter
+    rope = Rope()
+    for line in data:
+        print(line)
+        rope.move_and_follow(line[0], int(line[2]))
+    return 
 
 
 def part_two(data):
-    for letter in range(14, len(data)):
-        if len(set(data[letter-14:letter])) == 14:
-            return letter
+    pass
 
-
-# print(part_one(data))
-print(part_two(data))
+print(part_one(data))
+# print(part_two(data))
 
 
 
